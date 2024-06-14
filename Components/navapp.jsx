bcc17949a5636cfda8b6244ec8b394d4fcb520b9
abhay123123 <151843354+abhay123123@users.noreply.github.com>
2024-06-14 20:38:32 +0530

    Add files via upload

---
+++ Components/navapp.jsx
@@ -0,0 +1,20 @@
+import Navbar from "./navbar.jsx";
+
+
+function App(){
+
+    return(
+        <>
+        <Navbar></Navbar>
+        
+        
+        </>
+    )
+
+
+
+
+}
+
+
+export default App;
---
+++ Components/navbar.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+function Navbar(){
+    return(<>
+
+<header className="header">
+<a href="" className="logo">Logo</a>  
+<nav className="navbar">
+    <a href="a">Home</a>
+    <a href="a">About</a>
+    <a href="a">Portfolio</a>
+    <a href="a">Services</a>
+    <a href="a">Contact</a>
+
+
+
+
+
+
+
+
+
+</nav>
+</header>
+
+    </>
+
+    
+    );
+
+
+
+
+
+}
+
+
+
+export default Navbar;
